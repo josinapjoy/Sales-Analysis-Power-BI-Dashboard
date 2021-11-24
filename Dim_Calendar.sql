0f948772ba88aeb53e83552b30f34fe60281a110
Josina Palamoottil Joy <66690240+josinapjoy@users.noreply.github.com>
2021-11-24 11:36:06 +0100

    Add files via upload

---
+++ Dim_Calendar.sql
@@ -0,0 +1,16 @@
+use [AdventureWorksDW2019]
+-- Cleansing DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  [EnglishDayNameOfWeek] AS Day, 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,   -- Useful for front end date navigation and front end graphs.
+  [FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2019
